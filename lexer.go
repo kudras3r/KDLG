6eedd4a297c3bdcc836be1511154384df8a3d1df
kudras3r <d8880888@mail.ru>
2025-02-18 22:06:44 +0300

    lexer work

--- lexer.go
+++ lexer.go
@@ -1,35 +1,74 @@
 package main
 
+// TODO :
+// add line field to token ?
+// add types
+
 type tokenCat int
+
+var iMap = map[string]tokenCat{
+	"func":  FN,
+	"ret":   RET,
+	"for":   FOR,
+	"brk":   BRK,
+	"next":  NXT,
+	"if":    IF,
+	"main":  MAIN,
+	"else":  ELSE,
+	"x0r":   XOR,
+	"class": CLASS,
+}
+
+func LookupType(idn string) (tokenCat, bool) {
+	if iCat, ok := iMap[idn]; ok {
+		return iCat, true
+	}
+	return IDENT, false
+
+}
 
 const (
 	ILLEGAL tokenCat = iota
 	EOF
 
 	IDENT
-	INT
-	// TODO int32... float...
+	BYTE
+	INT16
+	INT32
+	INT64
+	FLOAT
+	BOOL
+	BOOLLIT
+	STR
 
-	ASSIGN
-	// TODO fast assign
-	PLUS // +
-	MIN  // -
-	DIV  // /
-	MUL  // *
+	ASSIGN  // =
+	FASSIGN // <<=
+	PLUS    // +
+	MIN     // -
+	DIV     // /
+	MUL     // *
+	REM     // %
 
-	EQ  // ==
-	NE  // !=
-	GT  // >
-	GTE // >=
-	LT  // <
-	LTE // <=
+	EQ   // ==
+	NE   // !=
+	GT   // >
+	GTE  // >=
+	LT   // <
+	LTE  // <=
+	AND  // &&
+	OR   // ||
+	INCR // +=
+	DECR // -=
 
-	COMMA  // ,
-	SCOLON // ;
-	LPAREN // (
-	RPAREN // )
-	LBRACE // {
-	RBRACE // }
+	DOT       // .
+	COMMA     // ,
+	SCOLON    // ;
+	LPAREN    // (
+	RPAREN    // )
+	LBRACE    // {
+	RBRACE    // }
+	LSBRACKET // [
+	RSBRACKET // ]
 
 	// keywords
 	MAIN
@@ -41,12 +80,13 @@ const (
 	BRK
 	NXT
 	XOR
+	CLASS
 )
 
-type token struct {
-	cat    tokenCat
-	lineno int
-	lex    string
+type Token struct {
+	cat tokenCat
+	col int
+	val string
 }
 
 type Lexer struct {
@@ -60,27 +100,147 @@ type Lexer struct {
 }
 
 func New(input string) *Lexer {
-	return &Lexer{
+	l := &Lexer{
 		sourse:    input,
 		sourseLen: len(input),
 	}
+	l.readCh()
+	return l
 }
 
-func newToken(cat tokenCat) token {
-	return token{
+func (l *Lexer) NextToken() Token {
+	var t Token
+	l.skipWS()
+	switch l.ch {
+	case '!':
+		if l.peekCh() == '=' {
+			l.readCh()
+			t = newToken(NE, l.col)
+		}
+	case '=':
+		if l.peekCh() == '=' {
+			l.readCh()
+			t = newToken(EQ, l.col)
+		} else {
+			t = newToken(ASSIGN, l.col)
+		}
+	case '+':
+		if l.peekCh() == '=' {
+			l.readCh()
+			t = newToken(INCR, l.col)
+		} else {
+			t = newToken(PLUS, l.col)
+		}
+	case '-':
+		if l.peekCh() == '=' {
+			l.readCh()
+			t = newToken(DECR, l.col)
+		} else {
+			t = newToken(MIN, l.col)
+		}
+	case '*':
+		t = newToken(MUL, l.col)
+	case '/':
+		t = newToken(DIV, l.col)
+	case '%':
+		t = newToken(REM, l.col)
+	case '<':
+		if l.peekCh() == '=' {
+			l.readCh()
+			t = newToken(LTE, l.col)
+
+		} else if l.peekCh() == '<' {
+			l.readCh()
+			if l.peekCh() == '=' {
+				t = newToken(FASSIGN, l.col)
+			}
+		} else {
+			t = newToken(LT, l.col)
+		}
+	case '>':
+		if l.peekCh() == '=' {
+			l.readCh()
+			t = newToken(GTE, l.col)
+		} else {
+			t = newToken(GT, l.col)
+		}
+	case '&':
+		if l.peekCh() == '&' {
+			l.readCh()
+			t = newToken(AND, l.col)
+		}
+	case '|':
+		if l.peekCh() == '|' {
+			l.readCh()
+			t = newToken(OR, l.col)
+		}
+	case ',':
+		t = newToken(COMMA, l.col)
+	case '.':
+		t = newToken(DOT, l.col)
+	case ';':
+		t = newToken(SCOLON, l.col)
+	case '(':
+		t = newToken(LPAREN, l.col)
+	case ')':
+		t = newToken(RPAREN, l.col)
+	case '{':
+		t = newToken(LBRACE, l.col)
+	case '}':
+		t = newToken(RBRACE, l.col)
+	case '[':
+		t = newToken(LSBRACKET, l.col)
+	case ']':
+		t = newToken(RSBRACKET, l.col)
+	case 0:
+		t = newToken(EOF, l.col)
+	default:
+		if isAlpha(l.ch) {
+			idn := l.getIdn()
+			tCat, ok := LookupType(idn)
+			if ok {
+				t = newToken(tCat, l.col)
+			} else {
+				t = newTokenWithVal(tCat, l.col, idn)
+			}
+			return t
+		} else if isNum(l.ch) {
+			// TODO : float ints...
+			n := l.getInt()
+			t = newTokenWithVal(INT64, l.col, n)
+		} else {
+			t = newToken(ILLEGAL, l.col)
+		}
+
+	}
+	l.readCh()
+	return t
+}
+
+func newToken(cat tokenCat, col int) Token {
+	return Token{
 		cat: cat,
+		col: col,
 	}
 }
 
-func newTokenWithVal(cat tokenCat, lex string) token {
-	return token{
+func newTokenWithVal(cat tokenCat, col int, val string) Token {
+	return Token{
 		cat: cat,
-		lex: lex,
+		col: col,
+		val: val,
 	}
 }
 
-func (l *Lexer) getNum() string {
-	// TODO float read
+func (l *Lexer) getFloat() string {
+	f := ""
+	for isNum(l.ch) || l.ch == '.' {
+		f += string(l.ch)
+	}
+	return f
+}
+
+func (l *Lexer) getInt() string {
 	n := ""
 	for isNum(l.ch) {
 		n += string(l.ch)
--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
 func main() {
+	s := "<= = . 8"
+	l := New(s)
+	for {
+		t := l.NextToken()
+		if t.cat == EOF {
+
+			break
+		}
+
+	}
 }
